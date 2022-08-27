88e3784f221938447da4b5ee099e8e86e236606b
dathoang <dathoang31082001@gmail.com>
2022-08-27 18:50:54 +0700

    update product with firebase

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../context/cart';
 
@@ -8,8 +9,11 @@ import CartItem from '../cart-item/cart-item';
 import './cart-dropdown.scss';
 
 const CartDropdown = () => {
+    const navigate = useNavigate();
     const { cartItems } = useContext(CartContext);
-
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
     return (
         <div className='cart-dropdown-container'>
             <div className='cart-items'>
@@ -21,7 +25,7 @@ const CartDropdown = () => {
                     <span className='empty-message'>Your cart is empty</span>
                 )}
             </div>
-            <Button>GO TO CHECKOUT</Button>
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     );
 };
--- src/context/products.jsx
+++ src/context/products.jsx
@@ -1,13 +1,20 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+// import { addCollectionAndDocuments } from '../utils/firebase/firebase';
 
-import PRODUCTS from '../shop-data.json';
+import SHOP_DATA from '../shop-data.js';
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(PRODUCTS);
+    const [products, setProducts] = useState([]);
+
+    // Chỉ dùng 1 lần đề update sản phẩm lên firebase
+    // useEffect(() => {
+    //     addCollectionAndDocuments('collections', SHOP_DATA);
+    // }, []);
+
     const value = { products };
     return (
         <ProductsContext.Provider value={value}>
