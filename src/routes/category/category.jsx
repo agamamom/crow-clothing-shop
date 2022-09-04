17759e68da3ae550587c315dce472e8bf033f075
dathoang <dathoang31082001@gmail.com>
2022-09-04 21:10:46 +0700

    update redux thunk

--- src/routes/category/category.jsx
+++ src/routes/category/category.jsx
@@ -1,30 +1,38 @@
 import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectCategoriesMap } from '../../store/categories/category.selector';
+import {
+    selectCategoriesMap,
+    selectIsLoading,
+} from '../../store/categories/category.selector';
+import Spinner from '../../components/spinner/spinner.component';
 import ProductCard from '../../components/product-card/product-card';
+import { CategoryContainer, Title } from './category.styles';
 
-
-import './category.scss';
+import './category.styles.jsx';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
     const [products, setProducts] = useState(categoriesMap[category]);
-
+    const isLoading = useSelector(selectIsLoading);
     useEffect(() => {
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
 
     return (
         <Fragment>
-            <h2 className='category-title'>{category.toUpperCase()}</h2>
-            <div className='category-container'>
-                {products &&
-                    products.map((product) => (
-                        <ProductCard key={product.id} product={product} />
-                    ))}
-            </div>
+            <Title>{category.toUpperCase()}</Title>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <CategoryContainer>
+                    {products &&
+                        products.map((product) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                </CategoryContainer>
+            )}
         </Fragment>
     );
 };
--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase';
-import { setCategories } from '../../store/categories/category.action';
+
 import CategoriesPreview from '../categories-preview/categories-preview';
 import Category from '../category/category';
+import { fetchCategoriesStartAsync } from '../../store/categories/category.action';
 
 import './shop.scss';
 
@@ -12,12 +12,9 @@ const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArray = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArray))
-        };
-        getCategoriesMap();
+        dispatch(fetchCategoriesStartAsync());
     }, []);
+
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
