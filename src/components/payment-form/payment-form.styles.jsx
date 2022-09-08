9bf3c7ce71a51243e9e4f599d97bdc34aba8335b
dathoang <dathoang31082001@gmail.com>
2022-09-08 21:39:49 +0700

    update

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -7,7 +7,8 @@ export const PaymentFormContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content:center;
+  color: red;
 `;
 
 export const FormContainer = styled.form`
